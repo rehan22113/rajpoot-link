86c76575892267801d3fe64406ef91e65ca821ee
Muhammad Rehan <raorehanweb@gmail.com>
2024-03-15 17:28:55 +0500

    new

--- src/Pages/Industry.jsx
+++ src/Pages/Industry.jsx
@@ -43,7 +43,7 @@ const Industry = () => {
           <div className="bg-gray-900 bg-opacity-70 flex items-end pb-10 h-full">
             <div className="px-6 max-w-xl">
               <h2 className="text-2xl text-white font-semibold">{item.name}</h2>
-              <p className="mt-2 text-gray-200">{category.length} Products</p>
+              {/* <p className="mt-2 text-gray-200">{category.length} Products</p> */}
               <Link to={`/industry/category?iId=${id}&cId=${item._id}/?pName=${industryName}&cName=${item.name}`} className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none">
                 <span>Explore Now</span>
                 <svg className="h-5 w-5 mx-2" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
