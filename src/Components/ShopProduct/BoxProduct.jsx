7a71731465524c16ad39daa24a8cdb19b90a889b
apricopt <raorehanweb@gmail.com>
2025-03-26 21:26:37 +0500

    updated

--- src/Components/ShopProduct/BoxProduct.jsx
+++ src/Components/ShopProduct/BoxProduct.jsx
@@ -11,7 +11,7 @@ const BoxProduct = ({item}) => {
             <div className="px-2 py-3">
            <div className=" rounded-lg text-center">
   <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{item.category?.name}</h3>
-  <Link to={`/product/${item.url}`}>
+  <Link to={`/product/${item._id}`}>
   <h2 className="text-lg hover:underline text-gray-900 font-medium title-font mb-4">{item.title}</h2>
   </Link>
 
